4fb9d3fe9814c0e41c831c983df6a98ca60d697f
wingillis <win.gillis@gmail.com>
2018-06-06 10:07:13 -0400

    added some boilerplate to set up the realsense recording streams

--- mousesense/main.py
+++ mousesense/main.py
@@ -8,13 +8,16 @@ def main():
   ctx = rs.context()
   devs = ctx.query_devices()
   print(f'There are {len(devs)} devices')
-  for i in devs:
-    print(i)
   print(f'Connecting to first device...')
   dev = devs[0]
+  print(dev)
   adv_mode = rs.rs400_advanced_mode(dev)
-  # print(f'Advanced mode is {adv_mode.is_enabled()}')
-  print(adv_mode)
+  print(f'Advanced mode is {adv_mode.is_enabled()}')
+  pipeline = rs.pipeline()
+  config = rs.config()
+  config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+  config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+  config.enable_stream(rs.stream.infrared, 640, 480, rs.format.y8, 30)
 
 def cli():
   fire.Fire(main)
